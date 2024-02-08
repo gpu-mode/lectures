9859c1e32dd0414c9ffb88f9265c529d0c5c1802
Jeremy Howard <j@fast.ai>
2024-02-08 11:24:01 +1000

    lecture5 updates

--- utils.py
+++ utils.py
@@ -3,14 +3,11 @@ import matplotlib.pyplot as plt
 from torch.utils.cpp_extension import load_inline
 
 def show_img(x, figsize=(4,3), **kwargs):
+    "Display HW or CHW format image `x`"
     plt.figure(figsize=figsize)
     plt.axis('off')
     if len(x.shape)==3: x = x.permute(1,2,0)  # CHW -> HWC
     plt.imshow(x.cpu(), **kwargs)
-
-def load_cuda(cuda_src, cpp_src, funcs, opt=False, verbose=False):
-    return load_inline(cuda_sources=[cuda_src], cpp_sources=[cpp_src], functions=funcs,
-                       extra_cuda_cflags=["-O2"] if opt else [], verbose=verbose, name="inline_ext")
 
 cuda_begin = r'''
 #include <torch/extension.h>
@@ -20,6 +17,24 @@ cuda_begin = r'''
 #define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
-
-inline unsigned int cdiv(unsigned int a, unsigned int b) { return (a + b - 1) / b;}
+#define CUDA_ERR(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess) 
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+__host__ __device__ inline unsigned int cdiv(unsigned int a, unsigned int b) { return (a+b-1)/b;}
 '''
+
+def load_cuda(cuda_src, cpp_src, funcs, opt=False, verbose=False, name=None):
+    "Simple wrapper for torch.utils.cpp_extension.load_inline"
+    if name is None: name = funcs[0]
+    return load_inline(cuda_sources=[cuda_src], cpp_sources=[cpp_src], functions=funcs,
+                       extra_cuda_cflags=["-O2"] if opt else [], verbose=verbose, name=name)
+
+def cdiv(a,b):
+    "Int ceiling division of `a` over `b`"
+    return (a+b-1)//b
